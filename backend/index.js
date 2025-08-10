503db756a7af036aa32ac20a12508764197c4e94
Natan Yidnekachew <nathanyidnekachew383@gmail.com>
2025-08-10 03:35:26 +0300

    Role based access control created

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const hospitalAPIRoutes = require('./routes/hospitalAPI');
 const superAdminAPIRoutes = require('./routes/super-adminAPI');
+const { verifyToken } = require('./middlewares/auth');
 
 app.use(express.json());
 
@@ -10,6 +11,12 @@ app.use('/api', hospitalAPIRoutes);
 
 // super-admin profile routes
 app.use('/api', superAdminAPIRoutes);
+
+// token verification route
+app.get('/test', verifyToken, (req, res) => {
+    res.status(200).json({ message: 'Token fully working', role: req.user });
+});
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
